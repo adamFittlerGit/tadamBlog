4ec3983d2ffd3552be7f9c76e5a925975bcd58b9
adamFittlerGit <fittleradam@gmail.com>
2024-11-11 20:28:08 +1000

    basic page UI added

--- frontend/app/components/topnav.tsx
+++ frontend/app/components/topnav.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
 
 const Topnav = () => {
   return (
-    <div>Topnav</div>
+    <div className='flex justify-center bg-lime-300 p-5'>
+        <div className='flex w-3/4 items-center'>
+            <div className='flex items-center'>
+                <Image 
+                    src="/blue-heart.png"
+                    width="50"
+                    height="50"
+                    alt="logo image" 
+                    className='mx-3'
+                />
+                <p className='text-black text-3xl font-bold'>Tadam Blog</p>
+            </div>
+            <div className='ml-auto'>
+                {/* I will update this later to change to that of the currently logged in user that being Tayla or I */}
+                <Link href="/login">
+                    <Image
+                        src="/login.png"
+                        width="50"
+                        height="50"
+                        alt="login image" 
+                        className='bg-white rounded-full border-black border-4'
+                    />
+                </Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
